12da72da3eb0fbf139fea2f073e8602347e06c51
HaciKandemir <hacikandemir2000@hotmail.com>
2022-08-11 19:34:09 +0300

    create - todoService

---
+++ src/Service/TodoService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Task;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class TodoService
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var TaskRepository
+     */
+    private $taskRepository;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        TaskRepository $taskRepository
+    ) {
+        $this->em = $em;
+        $this->taskRepository = $taskRepository;
+    }
+
+    public function addMultipleTask(array $tasks): void
+    {
+        foreach ($tasks as $taskData) {
+            $task = new Task();
+            $task->setName($taskData['name']);
+            $task->setDuration($taskData['duration']);
+            $task->setDifficulty($taskData['difficulty']);
+
+            $this->taskRepository->add($task);
+        }
+        $this->em->flush();
+    }
+}
